d8e89becfaf0c6057795a8ba6d008ffc34eb04c3
tvha <you@example.com>
2023-11-27 22:19:41 +0600

    Increase the font, since dpi is changed

--- config.def.h
+++ config.def.h
@@ -10,9 +10,9 @@ static const unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka Nerd Font Mono:size=18" };
-static const char dmenufont[]       = "Iosevka Nerd Font Mono:size=18";
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "Iosevka Nerd Font Mono:size=14" };
+static const char dmenufont[]       = "Iosevka Nerd Font Mono:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
